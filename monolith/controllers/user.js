c7f361f1f72ca00b696ade6beb1273327631afec
salemshah <salemshahdev@gmail.com>
2023-10-03 12:42:03 +0200

    first commit

---
+++ monolith/controllers/user.js
@@ -0,0 +1,25 @@
+class User {
+
+    /**
+     * @url = /auth
+     * @method=POST
+     * @param req
+     * @param res
+     */
+    auth(req, res) {
+        console.log("/auth")
+    }
+    /**
+     * @url = /auth
+     * @method= POST
+     * @param req
+     * @param res
+     */
+    auth(req, res) {
+        console.log("/register")
+    }
+
+
+}
+
+module.exports = User
